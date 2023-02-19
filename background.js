5766b1d23e56b4e61a4e636223378e14ffeb3c31
titooo7 <juanmartinbravo@gmail.com>
2023-02-19 19:30:11 +0100

    v2. input sent but not received by content script

--- background.js
+++ background.js
@@ -2,126 +2,143 @@
 // It listens for gamepad events and sends messages to content scripts
 
 // A function that maps gamepad buttons to keyboard or mouse events
-function mapGamepadButtons(gamepad) {
-  // Get an array of buttons from the gamepad object
-  let buttons = gamepad.buttons;
+function mapGamepadToMouseAndKeyboardEvents(gamepad) {
+  // Destructure the gamepad object to access the buttons and axes arrays
+  const { buttons, axes } = gamepad;
+
   // Create an empty array for storing messages
-  let messages = [];
-  // Loop through each button
-  for (let i = 0; i < buttons.length; i++) {
-    // Check if the button is pressed
-    if (buttons[i].pressed) {
-      // Switch on the button index
+  const messages = [];
+
+  // Map button presses to events
+  buttons.forEach((button, i) => {
+    if (button.pressed) {
       switch (i) {
         case 12: // D-Pad Up button
-          // Push a message with keyboard event data
           messages.push({
             type: "keyboard",
             key: "ArrowUp",
             keyCode: 38,
+            action: "move up"
           });
           break;
         case 13: // D-Pad Down button
-          // Push a message with keyboard event data
           messages.push({
             type: "keyboard",
             key: "ArrowDown",
             keyCode: 40,
+            action: "move down"
           });
           break;
         case 14: // D-Pad Left button
-          // Push a message with keyboard event data
           messages.push({
             type: "keyboard",
             key: "ArrowLeft",
             keyCode: 37,
+            action: "move left"
           });
           break;
         case 15: // D-Pad Right button
-          // Push a message with keyboard event data
           messages.push({
             type: "keyboard",
             key: "ArrowRight",
             keyCode: 39,
+            action: "move right"
           });
           break;
-        case 0 :// A button 
-        	// Push a message with mouse event data for left click 
-        	messages.push({ 
-        		type : "mouse", 
-        		button : "left", 
-        		clicks :1 
-        	}); 
-        	break; 
-        case1 :// B button  
-        	// Push a message with mouse event data for right click  
-        	messages.push({  
-        		type : "mouse",  
-        		button : "right",  
-        		clicks :1  
-        	});  
-        	break; 
-        case2 :// X button  
-        	// Push a message with mouse event data for middle click  
-        	messages.push({  
-        		type : "mouse",  
-        		button : "middle",  
-        		clicks :1  
-        	});  
-        	break; 
-        case3 :// Y button   
-        	// Push a message with mouse event data for double left click   
-        	messages.push({   
-        		type : "mouse",   
-        		button : "left",   
-        		clicks :2   
-        	});   
-        	break; 
-        case4:// Left bumper    
-			// Push a message with mouse event data for scrolling up    
-			messages.push({    
-				type:"mouse",    
-				scroll:"up"    
-			});    
-			break;    
-		case5:// Right bumper     
-			// Push a message with mouse event data for scrolling down     
-			messages.push({     
-				type:"mouse",     
-				scroll:"down"     
-			});     
-			break;     
+        case 0: // A button
+          messages.push({
+            type: "mouse",
+            button: "left",
+            clicks: 1,
+            action: "left click"
+          });
+          break;
+        case 1: // B button
+          messages.push({
+            type: "mouse",
+            button: "right",
+            clicks: 1,
+            action: "right click"
+          });
+          break;
+        case 2: // X button
+          messages.push({
+            type: "mouse",
+            button: "middle",
+            clicks: 1,
+            action: "middle click"
+          });
+          break;
+        case 3: // Y button
+          messages.push({
+            type: "mouse",
+            button: "left",
+            clicks: 2,
+            action: "double left click"
+          });
+          break;
+        case 4: // Left bumper
+          messages.push({
+            type: "mouse",
+            scroll: "up",
+            action: "scroll up"
+          });
+          break;
+        case 5: // Right bumper
+          messages.push({
+            type: "mouse",
+            scroll: "down",
+            action: "scroll down"
+          });
+          break;
+        default:
+          console.error(`Unknown button pressed: ${i}`);
       }
     }
+  });
+
+  // Map analog stick movements to mouse movements
+  const [leftStickX, leftStickY] = axes.slice(0, 2);
+  const [rightStickX, rightStickY] = axes.slice(2, 4);
+
+  if (Math.abs(leftStickX) > 0.1 || Math.abs(leftStickY) > 0.1) {
+    messages.push({
+      type: "mouse",
+      movementX: leftStickX * 10,
+      movementY: leftStickY * 10,
+      action: "move mouse"
+    });
   }
+
+  if (Math.abs(rightStickX) > 0.1 || Math.abs(rightStickY) > 0.1) {
+    messages.push({
+      type: "mouse",
+      movementX: rightStickX * 10,
+      movementY: rightStickY * 10,
+      action: "move mouse"
+    });
+  }
+
   return messages;
 }
 
+
 // A function that sends messages to content scripts using chrome.tabs.sendMessage
 function sendMessagesToContentScripts(messages) {
-  // Get the current active tab id using chrome.tabs.query
   chrome.tabs.query(
     { active: true, currentWindow: true },
     function (tabs) {
-	 if (tabs[0]) {
-
-      let tabId = tabs[0].id;
-      // Loop through each message in the array and send it to the content script of the active tab using chrome.tabs.sendMessage
-      for (let message of messages) {
-        chrome.tabs.sendMessage(tabId, message, function(response) {
-          console.log("Message sent:", message);
-          // Keep the port open by returning true in the callback function
-          return true;
+      if (tabs.length > 0) {
+        let tabId = tabs[0].id;
+        chrome.tabs.sendMessage(tabId, messages, function(response) {
+          console.log("Messages sent:", messages);
         });
+      } else {
+        console.error("No active tabs found.");
       }
-	} else {
-      console.error("No tabs matching the specified criteria were found.");
-    }
     }
   );
 }
-
-
 
 // Add an event listener for gamepadconnected events using window.addEventListener
 window.addEventListener("gamepadconnected", function (event) {
@@ -136,7 +153,7 @@ window.addEventListener("gamepadconnected", function (event) {
      if (!newGamepad.connected) {
        clearInterval(intervalId);
      } else {
-       let messages = mapGamepadButtons(newGamepad);
+       let messages = mapGamepadToMouseAndKeyboardEvents(newGamepad);
        sendMessagesToContentScripts(messages);
      }
    },100);
--- content.js
+++ content.js
@@ -1,29 +1,121 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  console.log("Received message from background.js: ", message);
+// This is a content script for the Chrome extension
+// It listens for messages from background scripts and performs mouse and keyboard actions
 
-   // Switch on the message type
-   switch (message.type) {
-     case "keyboard":
-       console.log("Simulating keyboard event with key " + message.key + " and keyCode " + message.keyCode);
-       simulateKeyboardEvent(message.key, message.keyCode);
-       break;
-     case "mouse":
-       console.log("Simulating mouse event with button " + message.button + " and " + message.clicks + " clicks");
-       if (message.button === "left") {
-         simulateMouseEvent("click", 0, message.clicks);
-       } else if (message.button === "right") {
-         simulateMouseEvent("contextmenu", 2, message.clicks);
-       } else if (message.button === "middle") {
-         simulateMouseEvent("auxclick", 1, message.clicks);
-       }
-       break;
-     case "scroll":
-       console.log("Simulating scroll event with direction " + message.scroll);
-       if (message.scroll === "up") {
-         simulateWheelEvent(-100);
-       } else if (message.scroll === "down") {
-         simulateWheelEvent(100);
-       }
-       break;
-   }
+let tabId = null;
+
+// A function that performs mouse actions based on messages received
+function performMouseAction(message) {
+  let { button, clicks, movementX, movementY, scroll } = message;
+  let options = {};
+
+  if (button) {
+    options.button = button;
+    options.clicks = clicks;
+  }
+
+  if (movementX || movementY) {
+    options.x = movementX;
+    options.y = movementY;
+  }
+
+  if (scroll === "up") {
+    options.deltaY = -100;
+  } else if (scroll === "down") {
+    options.deltaY = 100;
+  }
+
+  if (Object.keys(options).length > 0) {
+    chrome.runtime.sendMessage({ action: "performMouseAction", options });
+  }
+}
+
+// A function that performs keyboard actions based on messages received
+function performKeyboardAction(message) {
+  let { key, keyCode } = message;
+  let options = {};
+
+  if (key) {
+    options.type = "keydown";
+    options.key = key;
+  } else if (keyCode) {
+    options.type = "keydown";
+    options.keyCode = keyCode;
+  } else {
+    options.type = "keyup";
+  }
+
+  chrome.runtime.sendMessage({ action: "performKeyboardAction", options });
+}
+
+chrome.runtime.onMessage.addListener(function (messages, sender, sendResponse) {
+    console.log("Messages received:", messages);
+    console.log("Sender tab ID:", sender.tab.id);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        console.log("Active tab ID:", tabs[0].id);
+    });
+
+    messages.forEach((message) => {
+        switch (message.type) {
+            case "mouse":
+                performMouseAction(message);
+                break;
+            case "keyboard":
+                performKeyboardAction(message);
+                break;
+            default:
+                console.error(`Unknown message type: ${message.type}`);
+        }
+    });
+
+    // Send a response to acknowledge that messages were received
+    sendResponse({ received: true });
 });
+
+
+// Add an event listener for keydown events
+window.addEventListener("keydown", function (event) {
+  if (tabId === sender.tab.id) {
+    chrome.runtime.sendMessage({ action: "performKeyboardAction", options: { type: "keydown", key: event.key, keyCode: event.keyCode } });
+  }
+});
+
+// Add an event listener for keyup events
+window.addEventListener("keyup", function (event) {
+  if (tabId === sender.tab.id) {
+    chrome.runtime.sendMessage({ action: "performKeyboardAction", options: { type: "keyup" } });
+  }
+});
+
+// Add an event listener for mousemove events
+window.addEventListener("mousemove", function (event) {
+  if (tabId === sender.tab.id) {
+    chrome.runtime.sendMessage({ action: "performMouseAction", options: { x: event.movementX, y: event.movementY } });
+  }
+});
+
+// Add an event listener for mousedown events
+window.addEventListener("mousedown", function (event) {
+  if (tabId === sender.tab.id) {
+    let options = { button: event.button === 0 ? "left" : event.button === 1 ? "middle" : "right", clicks: event.detail };
+    chrome.runtime.sendMessage({ action: "performMouseAction", options });
+  }
+});
+
+// Add an event listener for mouseup events
+window.addEventListener("mouseup", function (event) {
+  if (tabId === sender.tab.id) {
+    chrome.runtime.sendMessage({ action: "performMouseAction", options: {} });
+  }
+});
+
+// Add an event listener for wheel events
+window.addEventListener("wheel", function (event) {
+let options = { deltaY: event.deltaY };
+chrome.runtime.sendMessage({ action: "performMouseAction", options });
+});
+
+// Send a message to the background script to confirm that the content script is running in the correct tab
+chrome.runtime.sendMessage({ action: "confirmTab" }, function (response) {
+console.log("Content script received response:", response);
+});
+
